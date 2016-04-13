d19d7ab6d451733fe233219e7f7e2562a60cb0d6
AMDraisma <tsobutter@gmail.com>
2016-04-13 09:35:44 +0200

    Added turn label Cleaned up code a bit

--- src/MasterController.java
+++ src/MasterController.java
@@ -3,9 +3,6 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-/**
- * Created by aj on 29-3-16.
- */
 
 public class MasterController extends JPanel {
     private JTextField invoerVakSelecterenX, invoerVakSelecterenY, invoerVakZetX, invoerVakZetY, tellerWit, tellerZwart;
@@ -21,109 +18,94 @@ public class MasterController extends JPanel {
     public MasterController( Bord bord, Dammen dammen ) {
         this.bord = bord;
         this.dammen = dammen;
-        this.beurt = new JLabel("");
         this.setLayout(new GridBagLayout());
         GridBagConstraints c = new GridBagConstraints();
 
+        this.beurt = new JLabel("");
+        c.fill = GridBagConstraints.HORIZONTAL;
+        // eigenschappen van constraints zoals weightx worden behouden
+        // hoeven dus maar eenmalig te worden ingesteld
+        c.weightx = 0.5;
+        c.gridwidth = 4;
+        c.gridy = 0;
+        this.add(beurt, c);
 
+        c.gridwidth = 2;
 
         invoerBeginPosLabel = new JLabel( "Begin Positie Steen"); //label invoer vakken begin positie steen
-        c.fill = GridBagConstraints.HORIZONTAL;
-        c.weightx = 0.5;
-        c.gridwidth = 2;
         c.gridx = 0;
-        c.gridy = 0;
+        c.gridy = 1;
         this.add(invoerBeginPosLabel, c);
 
         invoerEindPosLabel = new JLabel( "Eind Positie Steen"); //label invoervak eindpositie steen
-        c.fill = GridBagConstraints.HORIZONTAL;
-        c.weightx = 0.5;
-        c.gridwidth = 2;
         c.gridx = 2;
-        c.gridy = 0;
+        c.gridy = 1;
         this.add(invoerEindPosLabel, c);
 
-        invoerVakSelecterenX = new JTextField( 5 ); //invoervak begin positie x coordinaat
-        c.fill = GridBagConstraints.HORIZONTAL;
-        c.weightx = 0.5;
         c.gridwidth = 1;
+
+        invoerVakSelecterenX = new JTextField( 5 ); //invoervak begin positie x coordinaat
         c.gridx = 0;
-        c.gridy = 1;
+        c.gridy = 2;
         this.add(invoerVakSelecterenX, c);
 
         invoerVakSelecterenY = new JTextField( 5 ); //invoervak begin positie y coordinaat
-        c.fill = GridBagConstraints.HORIZONTAL;
-        c.weightx = 0.5;
         c.gridx = 1;
-        c.gridy = 1;
+        c.gridy = 2;
         this.add(invoerVakSelecterenY, c);
 
         invoerVakZetX = new JTextField( 5 ); //invoervak eind positie x coordinaat
-        c.fill = GridBagConstraints.HORIZONTAL;
-        c.weightx = 0.5;
         c.gridx = 2;
-        c.gridy = 1;
+        c.gridy = 2;
         this.add(invoerVakZetX, c);
 
         invoerVakZetY = new JTextField( 5 ); //invoervak eind positie y coordinaat
-        c.fill = GridBagConstraints.HORIZONTAL;
-        c.weightx = 0.5;
         c.gridx = 3;
-        c.gridy = 1;
+        c.gridy = 2;
         this.add(invoerVakZetY, c);
 
         tellerWit = new JTextField( 5 ); //geslagen stenenteller v/d witte stenen FIXME: 6-4-2016
         tellerWit.setEditable(false);
         tellerWit.setText( "Wit: 0" );
-        c.fill = GridBagConstraints.HORIZONTAL;
-        c.weightx = 0.5;
         c.gridx = 0;
-        c.gridy = 4;
+        c.gridy = 5;
         this.add(tellerWit, c);
 
         tellerZwart = new JTextField( 5 ); //geslagen stenenteller v/d zwarte stenen FIXME: 6-4-2016
         tellerZwart.setEditable(false);
         tellerZwart.setText( "Zwart: 0" );
-        c.fill = GridBagConstraints.HORIZONTAL;
-        c.weightx = 0.5;
         c.gridx = 1;
-        c.gridy = 4;
+        c.gridy = 5;
         this.add(tellerZwart, c);
 
-        verzetKnop = new JButton("Verzet Steen"); //verzet steen knop
-        c.fill = GridBagConstraints.HORIZONTAL;
-        c.weightx = 0.5;
         c.gridwidth = 5;
+
+        verzetKnop = new JButton("Verzet Steen"); //verzet steen knop
         c.gridx = 0;
-        c.gridy = 2;
+        c.gridy = 3;
         this.add(verzetKnop, c);
         verzetKnop.addActionListener( new KnopHandlerVerzetKnop() );
 
-        geslagenLabel = new JLabel("Geslagen Stenen"); //Geslagen Stenen label
-        c.fill = GridBagConstraints.HORIZONTAL;
-        c.weightx = 0.5;
         c.gridwidth = 2;
-        c.gridheight = 1;
+
+        geslagenLabel = new JLabel("Geslagen Stenen"); //Geslagen Stenen label
         c.gridx = 0;
-        c.gridy = 3;
+        c.gridy = 4;
         this.add(geslagenLabel, c);
-        
+
+        c.gridwidth = 1;
 
         verzetKnop = new JButton("Help"); //help knopjes
-        c.fill = GridBagConstraints.HORIZONTAL;
-        c.weightx = 0.5;
-        c.gridwidth = 1;
         c.gridx = 3;
-        c.gridy = 4;
+        c.gridy = 5;
         this.add(verzetKnop, c);
         verzetKnop.addActionListener( new KnopHandlerHelpKnop() );
 
-        restartKnop = new JButton("Restart"); //restart knopjes
-        c.fill = GridBagConstraints.HORIZONTAL;
-        c.weightx = 0.5;
         c.gridwidth = 2;
+
+        restartKnop = new JButton("Restart"); //restart knopjes
         c.gridx = 2;
-        c.gridy = 5;
+        c.gridy = 6;
         this.add(restartKnop, c);
         restartKnop.addActionListener( new KnopHandlerRestartKnop() );
 
